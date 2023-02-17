bbf1a909800874e84096da7aea19a16b7d9e87fd
rakibsarowar <rakibsarowar@gmail.com>
2023-02-18 04:10:50 +0600

    added validation code for card one

--- JS/index.js
+++ JS/index.js
@@ -71,18 +71,29 @@
 let serial = 0;
 
 // First Card ---------------------------------
-document.getElementById('btn-first-card').addEventListener('click', function() {
+document.getElementById('btn-first-card').addEventListener('click', function () {
   serial += 1;
   const titleName = getInnerText('title-first-card');
   const baseValue = getFieldValue('first-input');
   const heightValue = getFieldValue('Second-input');
+
+  if (
+    baseValue == "" ||
+    heightValue == "" ||
+    baseValue <= 0 ||
+    heightValue <= 0 ||
+    baseValue != Number ||
+    heightValue != Number
+  ) {
+    return alert('Please enter any valid number')
+  }
   const area = 0.5 * baseValue * heightValue;
 
   displayData(titleName, area);
 });
 
 // Second Card ---------------------------------
-document.getElementById('btn-second-card').addEventListener('click', function() {
+document.getElementById('btn-second-card').addEventListener('click', function () {
   serial += 1;
   const titleName = getInnerText('title-second-card');
   const widthValue = getFieldValue('first-input-second-card');
@@ -93,49 +104,49 @@ document.getElementById('btn-second-card').addEventListener('click', function() 
 });
 
 //Third Card --------------------------------------
-document.getElementById('btn-third-card').addEventListener('click',function(){
-    serial +=1;
-    const titleName = getInnerText('title-third-card')
-    const baseValue = getInnerText('base-innerText-parallelogram');
-    const heightValue = getInnerText('height-Text-parallelogram');
-    const area = parseInt(baseValue)* parseInt(heightValue);
+document.getElementById('btn-third-card').addEventListener('click', function () {
+  serial += 1;
+  const titleName = getInnerText('title-third-card')
+  const baseValue = getInnerText('base-innerText-parallelogram');
+  const heightValue = getInnerText('height-Text-parallelogram');
+  const area = parseInt(baseValue) * parseInt(heightValue);
 
-    displayData(titleName,area);
+  displayData(titleName, area);
 });
 
 //Fourth Card --------------------------------------
-document.getElementById('btn-fourth-card').addEventListener('click',function(){
-    serial +=1;
-    const titleName = getInnerText('title-fourth-card')
-    const baseValue = getInnerText('base-innerText-rhombus');
-    const heightValue = getInnerText('height-innerText-rhombus');
-    const area = 0.5* parseInt(baseValue)* parseInt(heightValue);
+document.getElementById('btn-fourth-card').addEventListener('click', function () {
+  serial += 1;
+  const titleName = getInnerText('title-fourth-card')
+  const baseValue = getInnerText('base-innerText-rhombus');
+  const heightValue = getInnerText('height-innerText-rhombus');
+  const area = 0.5 * parseInt(baseValue) * parseInt(heightValue);
 
-    displayData(titleName,area);
+  displayData(titleName, area);
 });
 
 //Fifth Card --------------------------------------
-document.getElementById('btn-fifth-card').addEventListener('click',function(){
-    serial +=1;
-    const titleName = getInnerText('title-fifth-card')
-    const perimetterValue = getInnerText('perimeter-innerText-pentagon');
-    const baseValue = getInnerText('base-innerText-pentagon');
-    const area = 0.5* parseInt(perimetterValue)* parseInt(baseValue);
+document.getElementById('btn-fifth-card').addEventListener('click', function () {
+  serial += 1;
+  const titleName = getInnerText('title-fifth-card')
+  const perimetterValue = getInnerText('perimeter-innerText-pentagon');
+  const baseValue = getInnerText('base-innerText-pentagon');
+  const area = 0.5 * parseInt(perimetterValue) * parseInt(baseValue);
 
-    displayData(titleName,area);
+  displayData(titleName, area);
 });
 
 //Six Card --------------------------------------
-document.getElementById('btn-six-card').addEventListener('click',function(){
-    serial +=1;
-    const titleName = getInnerText('title-six-card')
-    const axis1Value = getInnerText('axis1-innerText-ellipse');
-    const axis2Value = getInnerText('axis2-innerText-ellipse');
-    console.log(titleName, axis1Value, axis2Value)
+document.getElementById('btn-six-card').addEventListener('click', function () {
+  serial += 1;
+  const titleName = getInnerText('title-six-card')
+  const axis1Value = getInnerText('axis1-innerText-ellipse');
+  const axis2Value = getInnerText('axis2-innerText-ellipse');
+  console.log(titleName, axis1Value, axis2Value)
 
-    const area = (Math.PI* parseInt(axis1Value)* parseInt(axis2Value)).toFixed(3);
+  const area = (Math.PI * parseInt(axis1Value) * parseInt(axis2Value)).toFixed(3);
 
-    displayData(titleName,area);
+  displayData(titleName, area);
 });
 
 
@@ -152,7 +163,7 @@ function getInnerText(id) {
 function getFieldValue(id) {
   const value = document.getElementById(id).value;
   return value;
-} 
+}
 
 // function for display data.
 function displayData(nameOfShape, calculation) {
